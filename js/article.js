ba61a26d482c8f50b89d865c91200e85bad48e93
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-26 11:00:53 -0500

    get articles

--- js/article.js
+++ js/article.js
@@ -1,18 +1,19 @@
 var AJAXer = AJAXer || {}
 
-AJAXer.Article = function(id, title, url, point, author, time_past, num_comment){
+AJAXer.Article = function(id, title, url, score, user_email, created_at, comments_count, current_vote){
   this.id = id;
   this.title = title;
   this.url = url;
-  this.point = point;
-  this.author = author;
-  this.time_past = time_past;
-  this.num_comment = num_comment;
+  this.score = score;
+  this.user_email = user_email;
+  this.created_at = created_at;
+  this.comments_count = comments_count;
+  this.current_vote = current_vote;
 }
 
 AJAXer.Article.prototype.showContent = function(){
   var articleHTML = '<li id=article_' + this.id + '>';
   articleHTML += this.title;
-  articleHTML += '<div>' + this.point + 'points by' + this.author + ' ' + this.time_past + 'ago' + '|' + this.num_comment + 'comments';
+  articleHTML += '<div>' + this.score + 'points by' + this.user_email + ' ' + this.created_at + 'ago' + '|' + this.comments_count + 'comments';
   return articleHTML
 }
--- js/article_list.js
+++ js/article_list.js
@@ -12,3 +12,19 @@ AJAXer.ArticleList.init = function(getUrl, articleListEl){
 
   $('#new-article').submit(this.createArticle.bind(this));
 };
+
+
+AJAXer.ArticleList.getArticles = function(event){
+  var articlesHTML = ''
+  $.get( this.url, function(data){
+    var articles = data.articles;
+    for (var i = 0; i < articles.length; i++){
+      article = new AJAXer.article)articles[i].id,
+      articles[i].title, articles[i].
+      articlesHTML += article.showContent();
+    };
+
+    this.articleListEl.empty();
+    this.articleListEl.append(articlesHTML);
+  }.bind(this));
+}
