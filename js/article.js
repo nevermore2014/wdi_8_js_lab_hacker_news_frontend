c1eb5b746c11a6663a5c06944dcccd4fcd45605a
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-02-26 15:20:56 -0500

    some change

--- js/article.js
+++ js/article.js
@@ -1,4 +1,6 @@
-var AJAXer = AJAXer || {}
+var AJAXer = AJAXer || {};
+
+AJAXer.Vote = {};
 
 AJAXer.Article = function(id, title, url, score, user_email, created_at, comments_count, current_vote){
   this.id = id;
@@ -14,6 +16,54 @@ AJAXer.Article = function(id, title, url, score, user_email, created_at, comment
 AJAXer.Article.prototype.showContent = function(){
   var articleHTML = '<li id=article_' + this.id + '>';
   articleHTML += this.title;
-  articleHTML += '<div>' + this.score + 'points by' + this.user_email + ' ' + this.created_at + 'ago' + '|' + this.comments_count + 'comments';
+  articleHTML += '<div>' + this.score + 'points by ' + this.user_email + ' ' + this.created_at + ' ago ' + '| ' + this.comments_count + ' comments' + '<button class="upvote" id="upvote_' + this.id + '">Upvote</button>' + '<button class="upvote" id="downvote_' + this.id + '">Downvote</button>'
+  $('<li id="article_' + this.id + '">').click()
+  $('<li id="article_' + this.id + '">').trigger('click');
   return articleHTML
+
+  AJAXer.Vote.initVote($('#upvote'))
+}
+
+AJAXer.Vote.initVote = function(direction){
+  this.direction = direction;
+
+  $('#upvote' + this.id).click(this.upVote.bind(this));
+
+  $('#upvote').trigger('click');
+
+  $('#downvote' + this.id).click(this.downVote.bind(this));
+
+  $('#downvote').trigger('click');
+
+}
+
+AJAXer.Vote.upVote = function(){
+
+  $.ajax({
+    type: 'PATCH',
+    url: 'http://haxor-news-json.herokuapp.com/articles/:article_id/vote',
+    dataType: 'json',
+    data: { backdoor_user_id: 6, direction: 'up' },
+    success:function(response){
+      var vote_id_array = this.id.split("_"),
+          article_id = vote_id_array[1];
+      article = articles[article_id];
+      article.score += 1;
+    }
+  });
+}
+
+AJAXer.Vote.downVote = function(){
+  $.ajax({
+    type: 'PATCH',
+    url: 'http://haxor-news-json.herokuapp.com/articles/:article_id/vote',
+    dataType: 'json',
+    data: { backdoor_user_id: 6, direction: 'down' },
+    success:function(response){
+      var vote_id_array = this.id.split("_"),
+          article_id = vote_id_array[1];
+      article = articles[article_id];
+      article.score -= 1;
+    }
+  })
 }
--- js/article_list.js
+++ js/article_list.js
@@ -3,57 +3,64 @@ var AJAXer = AJAXer || {};
 AJAXer.ArticleList = {};
 
 AJAXer.ArticleList.init = function(getUrl, articleListEl){
-  this.url = getUrl.
+  this.url = getUrl,
   this.articleListEl = articleListEl;
 
-  $('#get-articles').click(this.AJAXer.getArticles.bind(this));
+  $('#get-articles').click(this.getArticles.bind(this));
 
   $('#get-articles').trigger('click');
 
   $('#new-article').submit(this.createArticle.bind(this));
+
+
 };
 
 
 AJAXer.ArticleList.getArticles = function(event){
-  var articlesHTML = ''
-  $.get( this.url, function(data){
-    var articles = data.articles;
-    for (var i = 0; i < articles.length; i++){
-      article = new AJAXer.article)articles[i].id,
-      articles[i].title, articles[i].url, article[i].score, article[i].user_email, article[i].created_at, article[i].comments_count, article[i].current_vote);
-      articlesHTML += article.showContent();
-    };
+  var articlesHTML = '',
+      response;
 
-    this.articleListEl.empty();
-    this.articleListEl.append(articlesHTML);
-  }.bind(this));
+  $.ajax({
+      type: 'GET',
+      url: 'http://haxor-news-json.herokuapp.com/articles',
+      dataType: 'json',
+      data: { backdoor_user_id: 6 }
+    }).done(function(response){
+      var articles = response.articles;
+      for (var i = 0; i < articles.length; i++){
+        article = new AJAXer.Article(articles[i].id,
+        articles[i].title, articles[i].url, articles[i].score, articles[i].user_email, articles[i].created_at, articles[i].comments_count, articles[i].current_vote);
+        articlesHTML += article.showContent();
+      };
+      this.articleListEl.empty();
+      this.articleListEl.append(articlesHTML);
+    }.bind(this));
 }
 
 AJAXer.ArticleList.createArticle = function(event){
 
   var $form = $(event.target),
       $title = $form.find("input[name='title']"),
-      $url = $form.find("input[name='url]"),
-      action = $form.attr('action'),
+      $url = $form.find("input[name='url']"),
       articleListEl = this.articleListEl;
       event.preventDefault();
-
   $.ajax({
-    type: 'GET',
+    type: 'POST',
     url: 'http://haxor-news-json.herokuapp.com/articles',
     dataType: 'json',
-    data: { backdoor_user_id: 6 }
-  }).done(function(response){
-    event.preventDefault();
-    var article = new AJAXer.Article(
-      response.article.id,
-      response.article.title,
-      response.article.url,
-      response.article.score,
-      response.article.user_email,
-      response.article.created_at,
-      response.article.comments_count,
-      response.article.current_vote);
-    articleListEl.append(append.showContent());
+    data: { backdoor_user_id: 6, article: { title: $title.val(), url: $url.val()}},
+    success: function(response){
+      // debugger;
+      var article = new AJAXer.Article(
+        response.article.id,
+        response.article.title,
+        response.article.url,
+        response.article.score,
+        response.article.user_email,
+        response.article.created_at,
+        response.article.comments_count,
+        response.article.current_vote);
+      articleListEl.append(article.showContent());
+    }
   });
 }
